0d4a838e25dae5412d4acec819f60483d9d4b1de
Szarlus <karolgoraus@gmail.com>
2016-06-10 06:57:50 +0200

    poprawka z mysql na mysqli

--- system/model.php
+++ system/model.php
@@ -8,13 +8,13 @@ class Model {
 	{
 		global $config;
 		
-		$this->connection = mysql_pconnect($config['db_host'], $config['db_username'], $config['db_password']) or die('MySQL Error: '. mysql_error());
-		mysql_select_db($config['db_name'], $this->connection);
+		$this->connection = mysqli_pconnect($config['db_host'], $config['db_username'], $config['db_password']) or die('MySQL Error: '. mysqli_error());
+		mysqli_select_db($config['db_name'], $this->connection);
 	}
 
 	public function escapeString($string)
 	{
-		return mysql_real_escape_string($string);
+		return mysqli_real_escape_string($string);
 	}
 
 	public function escapeArray($array)
@@ -45,17 +45,17 @@ class Model {
 	
 	public function query($qry)
 	{
-		$result = mysql_query($qry) or die('MySQL Error: '. mysql_error());
+		$result = mysqli_query($qry) or die('MySQL Error: '. mysqli_error());
 		$resultObjects = array();
 
-		while($row = mysql_fetch_object($result)) $resultObjects[] = $row;
+		while($row = mysqli_fetch_object($result)) $resultObjects[] = $row;
 
 		return $resultObjects;
 	}
 
 	public function execute($qry)
 	{
-		$exec = mysql_query($qry) or die('MySQL Error: '. mysql_error());
+		$exec = mysqli_query($qry) or die('MySQL Error: '. mysqli_error());
 		return $exec;
 	}
     
