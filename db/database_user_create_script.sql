d6b339604d85a7c6c2511cb474b0687bb6147b0b
Szarlus <karolgoraus@gmail.com>
2016-06-20 19:15:50 +0200

    Stworzenie modelu bazy danych i skryptów sql, dla bazy i użytkowników

---
+++ db/database_user_create_script.sql
@@ -0,0 +1,9 @@
+-- Creation of blog admin with all privileges, only for db modification
+CREATE USER theblog_admin@localhost identified by 'zaq1@WSX';
+
+GRANT ALL PRIVILEGES on theblog.* to theblog@localhost WITH GRANT OPTION;
+
+-- Creation of user for usual blog activities, with no privileges for db structure alteration
+CREATE USER theblog@localhost identified by 'strongpassword';
+
+GRANT SELECT, INSERT, UPDATE, DELETE on theblog.* to theblog@localhost;
